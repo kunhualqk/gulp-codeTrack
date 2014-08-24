7fd948b9fc69ffdd3cc0d2d9e462efa7940a6882
qingkun.liao <qingkun.liao@taobao.com>
2014-08-24 08:14:50 +0800

    支持数据的分组、自动分组、和埋点互斥功能

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*jslint node: true */
 var es = require('event-stream');
 var fs = require('fs');
+var path = require('path');
 
 function onString(file, callback) {
 	if (file.isBuffer()) {
@@ -42,26 +43,41 @@ module.exports = function (option) {
 		implant: function () {
 			return es.through(function (file) {
 				var self =this;
-				onData(option.baseDataPath,function(data){
+				this.pause();
+				onData(path.join(option.workPath,"baseData.json"),function(data){
+					//合并多个version的数据
+					var groupData={};
+					for(var key in data){
+						if(!data[key] || !data[key].totalNum){continue;}
+						var group=key.split("|",2)[1];
+						(groupData[group]||(groupData[group]={totalNum:0})).totalNum+=data[key].totalNum;
+					}
+					//计算最大的采样
+					var maxNum=0;
+					for(var key in groupData){
+						maxNum=Math.max(groupData[key].totalNum,maxNum);
+					}
 					onString(file, function (str) {
-						str = str.replace(/\.codeTrack\((.*)\)[\s;]*\/\/+([^\r\n]+)/g,function(s,param,comment){
+						str = str.replace(/\.codeTrack\((.*)\)(?:[\s;]*\/\/+([^\r\n]+))?/g,function(s,param,comment){
 							var params=/^\s*['"]([^'"]+)['"](?:\s*,['"]([^'"]*)['"])?/.exec(param),
 								map={},
 								num= 0,
 								value,
-								pvLev = 0;
-							list.push([{
+								pvLev = maxNum?Math.max(Math.round(Math.log(maxNum)/Math.log(2)),0):20;
+							if(!params){console.log(["track format error:",param]);}
+							list.push({
 								name:params[1],
 								datum:params[2],
+								file:path.relative(file.cwd,file.path),
 								comment:comment
-							}]);
-							for(var key in data){
+							});
+							for(var key in groupData){
 								if(key==params[1]){
-									pvLev = map._ = Math.round(Math.log(data[key].totalNum)/Math.log(2));
+									pvLev = map._ = Math.round(Math.log(groupData[key].totalNum)/Math.log(2));
 									num++;
 								}
-								if(key.indexOf(params[1]+"_")===0){
-									pvLev = map[key]=Math.round(Math.log(data[key].totalNum)/Math.log(2));
+								if(key.indexOf(params[1]+"|")===0){
+									pvLev = map[key]=Math.round(Math.log(groupData[key].totalNum)/Math.log(2));
 									num++;
 								}
 							}
@@ -75,22 +91,22 @@ module.exports = function (option) {
 							return "(" + (function () {
 								var trackMap = {}, firstName = "";
 								return function (pvLev, name, datumName, config) {
+									config = config||{};
+									if(config.reject)
+									{
+										if(typeof(config.reject!="object")){config.reject=[config.reject];}
+										for(var i=config.reject.length-1;i>=0;i--)
+										{
+											if(trackMap[config.reject[i]]){return;}//实现互斥功能
+										}
+									}
 									if (!firstName) {//记录第一个采样
 										firstName = name;
 									}
 									var now = new Date().valueOf();
 									trackMap[name] = now;//记录此采样，不管是否命中采样都需要记录
-									if (config.group) {//采样分组
-										name = name + "_" + config.group;
-									}
-									if (typeof(pvLev) == "object") {//pv参数分组
-										pvLev = pvLev[config.group || "_"] || 0;
-									}
-									var sampling = Math.round(Math.min(Math.pow(2, pvLev) / __sampleNumDaily, __maxSamplingRatio));
-									if (Math.floor(Math.random() * sampling) > 0) {
-										return;
-									}
-									//计算发送参数
+
+									// 计算时间参数
 									var t;
 									if (name == firstName) {
 										var startTime = window.g_config && g_config.startTime;
@@ -100,12 +116,27 @@ module.exports = function (option) {
 										t = now - trackMap[datumName || firstName] || trackMap[firstName];
 									}
 
+									switch(config.autoGroup){
+										case "time":
+											config.group=config.autoGroup+"_"+Math.floor(Math.log(t)/Math.log(2));
+											break;
+									}
+									if (config.group) {//采样分组
+										name = name + "|" + config.group;
+									}
+									if (typeof(pvLev) == "object") {//pv参数分组
+										pvLev = pvLev[config.group || "_"] || 0;
+									}
+									var sampling = Math.round(Math.max(Math.pow(2, pvLev) / (__sampleNumDaily||8192), 1/(__maxSamplingRatio||(1/16))));
+									if (Math.floor(Math.random() * sampling) > 0) {
+										return;
+									}
 
-									var url = __dataUrl,
+									var url = __dataUri,
 										msg = [
 										'[u' + url + ']',
 										'[t' + t + ']',
-										'[c' + name + ']',
+										'[c' + __version + '|' + name + ']',
 										'[r' + sampling + ']'
 									].join("");
 									var nick = "", result;
@@ -144,17 +175,21 @@ module.exports = function (option) {
 										return "("+option[p].toString()+")()";
 									case "string":
 										return "'"+option[p]+"'";
+									case "undefined":
+										return "undefined";
 									default:
 										return option[p];
 								}
 							}) + ")()";
 						})
 						file.contents = new Buffer(str);
-						self.emit('data', file)
+						self.emit('data', file);
+						self.resume();
 					})
 				});
 			},function(){
-				fs.writeFileSync(option.samplingInfoPath, JSON.stringify(list))
+				console.log(list.length);
+				fs.writeFileSync(path.join(option.workPath,"samplingInfo.json"), JSON.stringify(list,null,4))
 			})
 		},
 		sync: function(){
